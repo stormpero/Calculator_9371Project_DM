f4aa026c29d1185bb86ac3489f4d5a7f4dcc6e8f
FStrean <57074640+FStrean@users.noreply.github.com>
2020-03-17 03:59:33 +0300

    Сделал свою 1 прогу
    пока проверки на неотрицательность не делал, надо решить как будут записываться отрицательные числа(? первый бит отвечает за знак)

--- 9371Project_DM/Allfunc.h
+++ 9371Project_DM/Allfunc.h
@@ -8,3 +8,4 @@ int NZER_N_B(int a); //Старосельский Александр
 int ABS_Z_N(int a);  //Старосельский Александр
 
 vector<int> ADD_1N_N(vector<int> input); //Михаил Киришский
+vector<int> MUL_Nk_N(vector<int> N, vector<int> k); //Чиганов Даниил
---
+++ 9371Project_DM/MUL_Nk_N.cpp
@@ -0,0 +1,12 @@
+#include "Allfunc.h"
+
+vector <int>MUL_Nk_N (vector<int> N, vector<int> k)
+{
+	// Проверяем если в функцию передан пустой вектор
+	if (N.empty() || k.empty())
+		throw "Empty input";
+	//До тех пор пока i не равно k прибавлять в конец числа 0, что эквивалентно умножению на 10
+	for (vector <int>i(0); i != k; i = ADD_1N_N(i))
+		N.push_back(0);
+	return N;
+}
