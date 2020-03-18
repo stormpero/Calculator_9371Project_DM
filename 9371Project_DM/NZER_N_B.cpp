3f7afd4c677460e29071e48720460d809250ca9c
Sasha <staroselsky.S@yandex.ru>
2020-03-18 17:33:04 +0300

    fix

--- 9371Project_DM/NZER_N_B.cpp
+++ 9371Project_DM/NZER_N_B.cpp
@@ -2,7 +2,7 @@
 
 int NZER_N_B(vector<int> a)
 {
-	if ((a.size() == 2) && (a[1] == 0)) // Смотрим размер, и элемент
+	if ((a.size() == 1 | 2) && (a[a.size()] == 0)) // Смотрим размер, и элемент
 		return false;
 	else
 		return true;
