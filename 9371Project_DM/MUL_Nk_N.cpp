91ed2414520335e4ac28bf7b6cd19148513494a6
FStrean <57074640+FStrean@users.noreply.github.com>
2020-03-18 13:13:23 +0300

    fix

--- 9371Project_DM/MUL_Nk_N.cpp
+++ 9371Project_DM/MUL_Nk_N.cpp
@@ -7,7 +7,7 @@ vector <int>MUL_Nk_N(vector<int> N, vector<int> k)
 		throw "Empty input";
 	//Проверяем чтобы вводимые данные были натуральными числами
 	//До тех пор пока i не равно k прибавлять в конец числа 0, что эквивалентно умножению на 10
-	for (vector <int>i(0); i != k; i = ADD_1N_N(i))
+	for (vector <int>i = { 0 }; i != k; i = ADD_1N_N(i))
 		N.push_back(0);
 	return N;
 }
