83770a88ddbeb81f4c6f2dad74e61c733a70c7db
Sasha <staroselsky.S@yandex.ru>
2020-03-18 16:06:24 +0300

    add ADD_NN_N( Не работает)

--- 9371Project_DM/9371Project_DM.cpp
+++ 9371Project_DM/9371Project_DM.cpp
@@ -4,9 +4,10 @@
 int main()
 {
 	int sign = 1; //ѕеременна€ дл€ хранени€ знака числа(1 - минус, 0 - плюс)
-	vector <int> test = { 0,1,2,3,4 };
+	vector <int> test = { 1,2,3,4 };
+	vector <int> test1 = { 1,2,3,4 };
+	test = ADD_NN_N(test,test1);
 
-	test = MUL_ZM_Z(test);
 	for (int i = 0; i < 5; i++)
 		cout << test[i];
 	
