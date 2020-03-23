c751bf9b05c2b7351bb686ece5abb6cb45547ac8
FStrean <57074640+FStrean@users.noreply.github.com>
2020-03-23 23:35:14 +0300

    ыука

--- 9371Project_DM/LED_P_Q.cpp
+++
@@ -1,9 +0,0 @@
-#include "Allfunc.h"
-
-int LED_P_Q(vector<Drob> polynominal)
-{
-	if(polynominal.empty())
-		throw "Empty input";
-	else
-		return polynominal.size(); // старшая степень многочлена равна размеру вектора 
-}
