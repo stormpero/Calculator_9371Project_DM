f82bffb6868b83fc358cf2f6960af4dc4e5e3521
1Gvozd1 <62207561+1Gvozd1@users.noreply.github.com>
2020-04-01 16:52:08 +0300

    RED_QQ_Q update

--- 9371Project_DM/RED_QQ_Q.cpp
+++ 9371Project_DM/RED_QQ_Q.cpp
@@ -7,7 +7,7 @@ Drob RED_Q_Q(Drob a)
 	vector <int> num = ABS_Z_N(a.numerator); // Получаем абсолютную величину числителя
 	vector <int> dem = ABS_Z_N(a.denominator);// Получаем абсолютную величину знаменателя
 	vector <int> NOD = GCF_NN_N(num, dem);//Находим НОД дроби
-	while (NOD != one) {//Пока НОД не станет равен 1
+	while (NOD != one) {//Пока НОД не станет равен единице
 
 		num = DIV_ZZ_Z(num, NOD);// Получаем частное от деления числителя на НОД
 		dem = DIV_ZZ_Z(dem, NOD);// Получаем частное от деления знаменателя на НОД
