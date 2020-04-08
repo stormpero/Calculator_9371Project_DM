65969b043a489df1555bc9a7e25b29888774246d
1Gvozd1 <62207561+1Gvozd1@users.noreply.github.com>
2020-04-08 11:40:37 +0300

    new MAT_Q_SUM

--- Project_DM/MAT_Q_SUM.cpp
+++ Project_DM/MAT_Q_SUM.cpp
@@ -2,12 +2,15 @@
 
 vector<vector<int>> MAT_Q_SUM(vector<vector<int>> a, vector<vector<int>> b)
 {
-	vector<vector<int>> c(a.size());//добавляем двумерный вектор,в который будет вноситься результат сложения
-	for (int i = 0; i < a.size(); i++)
+	vector<vector<int>> c(a.size());
+	for (int i = 0; i < c.size(); i++)
 	{
-		c[i].resize(a.size());
-		for (int j = 0; j < a.size(); j++)
-			c[i][j] = a[i][j] + b[i][j];//сложение элементов матриц
+
+
+		c[i].resize((ADD_NN_N(a[i], b[i])).size());
+		c[i] = ADD_ZZ_Z(a[i], b[i]);
+
+
 	};
-	return(c);//возвращаем результирующую матрицу
+	return (c);
 }
